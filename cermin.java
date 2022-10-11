64f548b090ca66b5381b098a21ef02184908f8f3
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-11 13:29:22 +0700

    Add files via upload

---
+++ cermin.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gesa Rizky Nugraha
+ */
+public class cermin {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" ********************************* ");
+        System.out.println(" *Masukkan Nilai X ");
+        System.out.println(" ********************************* ");
+
+        double x = sc.nextInt();
+
+        System.out.println(" ******************************* ");
+        System.out.println(" *Masukkan Nilai Y ");
+        System.out.println(" ******************************* ");
+
+        double y = sc.nextInt();
+
+        double x1 = -y;
+        double y1 = -x;
+
+        System.out.println(" ******************************* ");
+        System.out.println(" *Maka X' = " + x1);
+        System.out.println(" *Maka Y' = " + y1);
+        System.out.println(" ******************************* ");
+    }
+    
+}
